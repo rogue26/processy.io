fff321c889f276f505cf787c2dddec874ec7e93f
rogue26 <max.hill@pm.me>
2020-12-28 22:13:56 -0500

    various improvements

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'replace_in_production'  # os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
